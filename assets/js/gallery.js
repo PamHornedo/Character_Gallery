9196f8d7f504be30980861eb85b6e2fa21288146
Pamela Hornedo <pamhornedo@gmail.com>
2025-09-19 23:29:15 -0400

    added for loop for information already in localStorage

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -7,23 +7,42 @@ const characterImage = document.getElementById("imgUrl");
 const text = document.querySelector(".textInput");
 const image = document.querySelector(".imageInput");
 
+// Getting information from localStorage
 const getSavedData = localStorage.getItem("userInput");
 const savedData = JSON.parse(getSavedData);
+
+// Creating div and image elements for values from localStorage
+for (const key in savedData) {
+  let value = savedData[key];
+  if (key === "imgUrl") {
+    const newImg = document.createElement("img");
+    newImg.src = value;
+    image.appendChild(newImg);
+  } else {
+    const newDiv = document.createElement("div");
+    let content = `${key}: ${value}<br>`;
+    newDiv.innerHTML = content;
+    text.appendChild(newDiv);
+  }
+}
 
 // Listening for form submission
 characterForm.addEventListener("submit", function (event) {
   event.preventDefault();
   console.log("The form has been submitted.");
 
+  // Making inputted data into an object
   const form = event.target;
   const formData = new FormData(form);
   const dataObject = Object.fromEntries(formData.entries());
   console.log(dataObject);
 
+  // Turning objects into string and putting into localStorage
   const formDataString = JSON.stringify(dataObject);
 
   localStorage.setItem("userInput", formDataString);
 
+  // Creating div and image elements for inputted values
   for (const [key, value] of formData) {
     if (key === "imgUrl") {
       const newImg = document.createElement("img");
